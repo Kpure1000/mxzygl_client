a11902cf21cdb6ea490ecbd1fc213f5ff4b3d811
Kpure1000 <ktsq2000@126.com>
2023-05-05 14:25:08 +0800

    update: reduce job thread num

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     ConfigManager::getInstance("conf.ini");
-    JobSystem::getInstance(get_core_numbers());
+    JobSystem::getInstance(get_core_numbers() - 2);
 
     auto homeWin = std::make_shared<HomeWindow>(nullptr, &a);
 //    homeWin->showMaximized();
